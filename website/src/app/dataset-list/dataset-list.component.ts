342a75b1c6e083fdbf4daf6d59e76d88e719fb72
Ng Zhao Ming <ngzhaoming@gmail.com>
2020-03-31 14:17:49 +0800

    :sparkles: Add database formatting into website

--- website/src/app/dataset-list/dataset-list.component.ts
+++ website/src/app/dataset-list/dataset-list.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
       <div class="jumbotron jumbotron-fluid">
         <div class="container">
           <h1 class="display-4 database-title">Diabolical Database's Datasets 📚</h1>
-          <p class="lead database-title">Contains all the datasets which are obtained and adapted from</p>
+          <p class="lead database-title">Contains all the datasets which are obtained and adapted from various websites.</p>
         </div>
       </div>
+
+    <div class="card">
+      <div class="card-header">
+        All featured databases:
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Click on the link to be directed to the various datasets</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
     </div>
   `,
   styles: []
